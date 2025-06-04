f1302f9d06154f55b5f4b08e4f6f884ef47fa653
DAKSHITA VAIRAGI <dakshitavairagi72@gmail.com>
2025-06-04 17:40:28 +0530

    Delete src directory

--- src/components/ui/button.js
+++
@@ -1,6 +0,0 @@
-
-export const Button = ({ children, className, variant = "default", onClick }) => (
-  <button className={`px-4 py-2 rounded-xl ${variant === "secondary" ? "bg-white text-black" : "bg-blue-600 text-white"} ${className}`} onClick={onClick}>
-    {children}
-  </button>
-);
